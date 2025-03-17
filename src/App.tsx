1b1eacef50317eddbdd51e9b0e055940b8fa1178
Qwixo <101059562+Qwixo@users.noreply.github.com>
2025-03-17 05:18:43 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import LandingPage from './pages/LandingPage'
+import GuidePage from './pages/GuidePage'
+import ScrollToTop from './components/ScrollToTop'
+
+function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/guide" element={<GuidePage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
